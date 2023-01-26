433814a07f3cfe8a3388a58eec0dec2e2546661c
emonShimoul <emonshimoul@gmail.com>
2023-01-26 15:22:24 +0600

    fetch image data from api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import SearchBar from './components/SearchBar';
+import searchImages from './api';
 
 function App() {
   const handleSubmit = (term) => {
-    console.log('Do a Search with', term);
+    // console.log('Do a Search with', term);
+    searchImages(term);
   }
 
   return (
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SearchBar = ({ onSubmit }) => {
+    const [term, setTerm] = useState('');
+
     const handleFormSubmit = (e) => {
         e.preventDefault();
 
-        onSubmit('cars');
+        onSubmit(term);
+    }
+
+    const handleChange = (e) => {
+        setTerm(e.target.value);
     }
 
     return (
         <div>
             <form onSubmit={handleFormSubmit}>
-                <input type="text" />
+                <input value={term} onChange={handleChange} />
             </form>
         </div>
     );
